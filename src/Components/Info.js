0152223eb800c60ea710698eda67bb386e69d32a
Hasibullah khan <Hasibpak@gmail.com>
2021-05-12 21:27:33 +0400

    update code in App.js Header.js and Info.js

---
+++ src/Components/Info.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Info() {
+    return (
+        <div>
+            <h2 style={{ color: "#4a54f1" }}>Student Information</h2>
+        </div>
+    )
+}
+export default Info;
