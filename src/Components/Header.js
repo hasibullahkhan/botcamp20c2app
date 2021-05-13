5a584fa8fee22cff21c3d9b687743658ce2c4eed
Hasibullah khan <Hasibpak@gmail.com>
2021-05-13 10:59:52 +0400

    add props line in Header.js and pass props installa in app.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,8 @@ function Head(props) {
             <p style={{ color: 'green', fontSize: 18, fontWeight: 'bold', marginLeft: 15 }}>{props.desc2}</p>
             <p style={{ color: 'green', fontSize: 18, fontWeight: 'bold', marginLeft: 15 }}>{props.desc3}</p>
             <h2 style={{ color: "#4a54f1", marginLeft: 15, textDecoration: "underline" }}>Install Reacts </h2>
-            <p style={{ color: 'green', fontSize: 18, fontWeight: 'bold', marginLeft: 15 }}>{props.install}</p>
+            <p style={{ color: 'green', fontSize: 18, fontWeight: 'bold', marginLeft: 15 }}>{props.installq}</p>
+            <p style={{ color: 'green', fontSize: 18, fontWeight: 'bold', marginLeft: 15 }}>{props.installa}</p>
         </div>
     )
 }
